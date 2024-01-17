d2227149298985c3cdced09ee56d16bce9745fc1
nbk121 <109231827+nbk121@users.noreply.github.com>
2024-01-17 18:01:23 +0100

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const knex = require('knex');
+const cors = require('cors')
+const cars = require('./cars')
+const dealers = require('./dealers')
+const auth = require('./auth')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: 'localhost',
+    user: 'postgres',
+    password: 'Alienilahet2005',
+    database: 'car_shop',
+  },
+});
+
+const app = express()
+app.use(cors())
+
+
+app.use(bodyParser.json())   
+
+
+
+app.get('/', (req, res) => { res.send(200).json("Server is up and running!") })
+
+app.post('/sign-up', (req, res) => auth.signUp(db)(req, res));
+app.post('/log-in', (req, res) => auth.logIn(db)(req, res));
+
+app.post('/cars', (req, res) => cars.createCar(db)(req, res))
+app.get('/cars', (req, res) => cars.readAll(db)(req, res))
+app.get('/cars/:id', (req, res) => cars.readCar(db)(req, res))
+app.put('/cars/:id', (req, res) => cars.updateCar(db)(req, res))
+app.delete('/cars/:id', (req, res) => cars.deleteCar(db)(req, res))
+app.post('/dealers', (req, res) => dealers.createDealer(db)(req, res))
+app.get('/dealers', (req, res) => dealers.readAll(db)(req, res))
+app.get('/dealers/:id', (req, res) => dealers.readDealer(db)(req, res))
+app.put('/dealers/:id', (req, res) => dealers.updateDealer(db)(req, res))
+app.delete('/dealers/:id', (req, res) => dealers.deleteDealer(db)(req, res))
+
+
+
+
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).json({ error: err.message })
+})
+
+
+app.listen(3000, () => console.log("Server is running on port 3000"))
+
